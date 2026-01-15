559ed8982f936cbd073d204689da0dafd536a27e
Chhavi Ganvir <ganvirchhavi@gmail.com>
2026-01-15 23:47:12 +0530

    Abstra program

---
+++ Day3_Program2/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day3_Program2
+{
+    abstract class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public abstract void DisplayProduct_Name_Details()
+   }
+      
+    
+
+    void displayProductDetailsonly()
+        {
+            Console.WriteLine("product name" + ProductName);
+        }
+    }
+}
+
---
+++ Day3_Program2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ElectronicProduct newObj1 = new ElectronicProduct();
+newObj1.ProductID = 101;
+newObj1.ProductName = "TV";
+newObj1.DisplayProduct_Name_Details();
+newObj1.ShowElectronicProductDetails();
+Console.WriteLine("Abstract classs in action");
+
+
+class ElectronicProduct:Product
+{
+    public override void DisplayProduct_Name_Details()
+    {
+        Console.WriteLine("Electronic Product name:" + ProductName);
+        Console.WriteLine("Electronic Product name:" + ProductID);
+    }
+    public void ShowElectronicProductDetails()
+    {
+        Console.WriteLine("This is an electronic product from child class");
+    }
+}
+
+
